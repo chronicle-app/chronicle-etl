70b2615992e203a47c6ec30cabc12e63e25a91ed
Andrew Louis <andrew@hyfen.net>
2020-08-02 14:54:28 -0400

    Adds ability to load an etl class from an external gem

--- lib/chronicle/etl/runner.rb
+++ lib/chronicle/etl/runner.rb
@@ -1,4 +1,10 @@
 class Chronicle::Etl::Runner
+  BUILTIN = {
+    extractor: ['stdin', 'json', 'csv'],
+    transformer: ['null'],
+    loader: ['stdout', 'csv', 'table']
+  }.freeze
+
   def initialize(options)
     @options = options
 
@@ -25,13 +31,27 @@ class Chronicle::Etl::Runner
   private
 
   def instantiate_etl_classes
-    @extractor = get_etl_class(:extractor, @options[:extractor][:name]).new(@options[:extractor][:options])
-    @transformer = get_etl_class(:transformer, @options[:transformer][:name]).new(@options[:transformer][:options])
-    @loader = get_etl_class(:loader, @options[:loader][:name]).new(@options[:loader][:options])
+    @extractor = load_etl_class(:extractor, @options[:extractor][:name]).new(@options[:extractor][:options])
+    @transformer = load_etl_class(:transformer, @options[:transformer][:name]).new(@options[:transformer][:options])
+    @loader = load_etl_class(:loader, @options[:loader][:name]).new(@options[:loader][:options])
   end
 
-  def get_etl_class(phase, name)
-    klass_name = "Chronicle::Etl::#{phase.to_s.capitalize}s::#{name.capitalize}"
+  def load_etl_class(phase, name)
+    if BUILTIN[phase].include? name
+      klass_name = "Chronicle::Etl::#{phase.to_s.capitalize}s::#{name.capitalize}"
+    else
+      # TODO: come up with syntax for specifying a particular extractor in a provider library
+      # provider, extractor = name.split(":")
+      provider = name
+      begin
+        require "chronicle/#{provider}"
+      rescue LoadError => e
+        warn("Error loading #{phase} '#{provider}'")
+        warn("  Perhaps you haven't installed it yet: `$ gem install chronicle-#{provider}`")
+        exit(false)
+      end
+      klass_name = "Chronicle::#{name.capitalize}::Extractor"
+    end
     Object.const_get(klass_name)
   end
 end
