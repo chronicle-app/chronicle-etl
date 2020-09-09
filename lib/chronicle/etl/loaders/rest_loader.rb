012ffe94ce5caf773ad12d1e56c203bc0276fe4d
Andrew Louis <andrew@hyfen.net>
2020-09-09 15:48:24 -0400

    Allows RestLoader to send request to https endpoint

--- lib/chronicle/etl/loaders/rest_loader.rb
+++ lib/chronicle/etl/loaders/rest_loader.rb
@@ -5,7 +5,7 @@ require 'json'
 module Chronicle
   module ETL
     class RestLoader < Chronicle::ETL::Loader
-      def initialize(options={})
+      def initialize( options={} )
         super(options)
       end
 
@@ -16,14 +16,17 @@ module Chronicle
           "Authorization" => "Bearer #{@options[:access_token]}",
           "Content-Type": 'application/json'
         }
+        use_ssl = uri.scheme == 'https'
 
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Post.new(uri.request_uri, header)
+        Net::HTTP.start(uri.host, uri.port, use_ssl: use_ssl) do |http|
+          request = Net::HTTP::Post.new(uri.request_uri, header)
 
-        obj = {data: result} unless result[:data]
-        request.body = obj.to_json
+          # have the outer data key that json-api expects
+          obj = { data: result } unless result[:data]
+          request.body = obj.to_json
 
-        response = http.request(request)
+          http.request(request)
+        end
       end
     end
   end
