df9c8192ae46745f08932a40e69b37fc52f8a5d4
Andrew Louis <andrew@hyfen.net>
2022-03-04 14:52:34 -0500

    Add some inverse associations for Entity

--- lib/chronicle/etl/models/entity.rb
+++ lib/chronicle/etl/models/entity.rb
@@ -6,12 +6,18 @@ module Chronicle
       class Entity < Chronicle::ETL::Models::Base
         TYPE = 'entities'.freeze
         ATTRIBUTES = [:title, :body, :provider_url, :represents, :slug, :myself, :metadata].freeze
+
+        # TODO: This desperately needs a validation system
         ASSOCIATIONS = [
+          :involvements, # inverse of activity's `involved`
+
           :attachments,
           :abouts,
+          :aboutables, # inverse of above
           :depicts,
           :consumers,
-          :contains
+          :contains,
+          :containers # inverse of above
         ].freeze  # TODO: add these to reflect Chronicle Schema
 
         attr_accessor(*ATTRIBUTES, *ASSOCIATIONS)
