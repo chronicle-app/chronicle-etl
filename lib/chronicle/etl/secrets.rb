6d8692c8ac5de7961a588fe561bcaf27b3bed780
Andrew Louis <andrew@hyfen.net>
2022-03-24 17:52:57 -0400

    Use module_function in Secrets

--- lib/chronicle/etl/secrets.rb
+++ lib/chronicle/etl/secrets.rb
@@ -2,22 +2,24 @@ module Chronicle
   module ETL
     # Secret management module
     module Secrets
+      module_function
+
       # Save a setting to a namespaced config file
-      def self.set(namespace, key, value)
+      def set(namespace, key, value)
         config = read(namespace)
         config[key.to_sym] = value
         write(namespace, config)
       end
 
       # Remove a setting from a namespaced config file
-      def self.unset(namespace, key)
+      def unset(namespace, key)
         config = read(namespace)
         config.delete(key.to_sym)
         write(namespace, config)
       end
 
       # Retrieve all secrets from all namespaces
-      def self.all(namespace = nil)
+      def all(namespace = nil)
         namespaces = namespace.nil? ? available_secrets : [namespace]
         namespaces
           .to_h { |namespace| [namespace.to_sym, read(namespace)] }
@@ -25,28 +27,27 @@ module Chronicle
       end
 
       # Return whether a namespace name is valid (lowercase alphanumeric and -)
-      def self.valid_namespace_name?(namespace)
+      def valid_namespace_name?(namespace)
         namespace.match(/^[a-z0-9\-]+$/)
       end
 
       # Read secrets from a config file
-      def self.read(namespace)
+      def read(namespace)
         definition = Chronicle::ETL::Config.load("secrets", namespace)
         definition[:secrets] || {}
       end
 
       # Write secrets to a config file
-      def self.write(namespace, secrets)
+      def write(namespace, secrets)
         data = {
-          provider: namespace,
           secrets: (secrets || {}).transform_keys(&:to_s),
           chronicle_etl_version: Chronicle::ETL::VERSION
-        }.transform_keys(&:to_s) # Should I implement deeply_transform_keys ?
+        }.transform_keys(&:to_s) # Should I implement deeply_transform_keys...?
         Chronicle::ETL::Config.write("secrets", namespace, data)
       end
 
       # Which config files are available in ~/.config/chronicle/etl/secrets
-      def self.available_secrets
+      def available_secrets
         Chronicle::ETL::Config.available_configs('secrets')
       end
     end
